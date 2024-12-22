8878820aee4f4e9256492454bd8c0e001a6aaeb4
Milad Naeini <milad88884@gmail.com>
2024-12-22 13:27:20 +0100

    feat: added Button, Card, and Textarea components from ShadCN using CLI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "@/components/ui/card"
+import { Textarea } from "@/components/ui/textarea"
 
 export default function Home() {
   return (
-   <>
-   <div className="py-2">
-    Hello World
+   <div className="p-5">
+    <Card>
+    <CardHeader>Hello World</CardHeader>
+      <CardContent>
+      <Textarea />
+
+   <Button>Send</Button>
+   <Button>Draft</Button>
+
+      </CardContent>
+   </Card>
    </div>
-   </>
   );
 }
