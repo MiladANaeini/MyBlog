7ae0f00b1a613dea6dfd196768379fd8480a4aeb
Milad Naeini <milad88884@gmail.com>
2025-01-01 11:07:35 +0100

    feat: display empty message when no posts are available
    - Added a user-friendly message to inform users when no posts exist.
    - Ensures better user experience by providing feedback in empty states.

--- src/components/blogPosts.tsx
+++ src/components/blogPosts.tsx
@@ -2,18 +2,35 @@ import { fetchData } from "@/common/helper/endpoint";
 import { PostType } from "@/types/global";
 import { BlogCard } from "./blogCard";
 import { Card, CardHeader } from "./ui/card";
+import Link from "next/link";
 
 export default async function BlogPosts() {
   const data = await fetchData();
 
   return (
     <section className="p-16">
-    <Card className="mt-3 p-8">
-      <CardHeader>Posted Blogs</CardHeader>
-      {data?.map((item: PostType) => (
-        <BlogCard key={item.id} item={item} />
-      ))}
-    </Card>
+      {data && (
+        <Card className="mt-3 p-8">
+          <CardHeader>Posted Blogs</CardHeader>
+          {data.length ? (
+            <>
+              {data.map((item: PostType) => (
+                <BlogCard key={item.id} item={item} />
+              ))}
+            </>
+          ) : (
+            <div className="bg-yellow-100 mt-3 p-4">
+              No blog posts have been created.
+              <Link href="/auth/signin">
+                <span className="text-sm font-bold text-blue-500">
+                  Sign In{" "}
+                </span>
+              </Link>
+              to start creating posts.
+            </div>
+          )}
+        </Card>
+      )}
     </section>
   );
 }
